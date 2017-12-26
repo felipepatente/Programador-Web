4c730363b21e01dd04870b6a5baa81cc6149ce7e
felipepatente <felipepatente@yahoo.com.br>
2017-12-26 21:32:16 -0200

    Fazendo uma consulta no banco

--- Capítulo 05 - Contrução de um Banco de Dados com MySQL/cadastromatricula.php
+++ Capítulo 05 - Contrução de um Banco de Dados com MySQL/cadastromatricula.php
@@ -1,9 +1,29 @@
-<?php
-	$nome = $_POST['aluno'];
-	$curso = $_POST['curso'];
-	require("conexao.php");
-	mysql_select_db("sis_academico");
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro Matricula</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		$nome = $_POST['aluno'];
+		$curso = $_POST['curso'];
+		require("conexao.php");
+		mysql_select_db("sis_academico");
 
-	$inserir = "INSERT INTO matricula (id,nome,curso) VALUES (DEFAULT,'$nome','$curso');";
-	mysql_query($inserir);
-?>
+		$inserir = "INSERT INTO matricula (id,nome,curso) VALUES (DEFAULT,'$nome','$curso');";
+		mysql_query($inserir);
+
+		//Lendo dados do banco
+		$consulta = "SELECT id, nome, curso FROM matricula";
+		$matricula = mysql_query($consulta);
+
+		
+		while ($array = mysql_fetch_array($matricula)) {
+			echo $array['id'] . " - " . " Aluno: " . $array['nome'] . " Curso: " . $array['curso'] . "<br>";
+						
+		}
+
+	?>
+</body>
+</html>
