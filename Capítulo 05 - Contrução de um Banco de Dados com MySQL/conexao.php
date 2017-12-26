12219f85a40b781a521a2cf138f40c1bbb83728e
felipepatente <felipepatente@yahoo.com.br>
2017-12-26 20:22:04 -0200

    Exemplo de conexão com o banco

--- Capítulo 05 - Contrução de um Banco de Dados com MySQL/conexao.php
+++ Capítulo 05 - Contrução de um Banco de Dados com MySQL/conexao.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Conexao</title>
+	<meta charset="utf-8">
 </head>
 <body>
+	<?php
+		$conexao = mysql_connect("localhost","root","");
+		if(!$conexao){
+			die('Não foi possível conectar ao banco de dados. Erro detectado: ' . mysql_error());
+		}
+		echo "Conexão bem-sucedida.";
+		mysql_set_charset('utf-8',$conexao);
 
+	?>
 </body>
 </html>
