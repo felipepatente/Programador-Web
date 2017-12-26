f082263f06e47385f36bf62e0aa2e94cb83d5056
felipepatente <felipepatente@yahoo.com.br>
2017-12-26 20:55:25 -0200

    Criando um simples formulario

---
+++ Capítulo 05 - Contrução de um Banco de Dados com MySQL/matricula.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro de Curso</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h2>Cadastro de curso</h2>
+	<p>Prezado(a) aluno(a), preencha com seus dados: </p>
+	
+	<form method="post" action="cadastrocurso.php">
+		<p>
+			<label for="aluno">Nome:</label>
+			<input type="text" id="aluno" name="aluno">
+		</p>
+
+		<p>
+			<label for="curso">Curso</label>
+			<input type="text" id="curso" name="curso">
+		</p>
+
+		<p>
+			<input type="reset" value="Limpar" name="limpar">
+			<input type="submit" value="Enviar" name="submit">
+		</p>
+		
+	</form>
+</body>
+</html>
