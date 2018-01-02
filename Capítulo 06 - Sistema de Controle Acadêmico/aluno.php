fe17bd7125cf85b52ddaf64aab28750d116cc838
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 20:20:06 -0200

    Colocando os campos de nome e cpf no formulário

---
+++ Capítulo 06 - Sistema de Controle Acadêmico/aluno.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro Aluno</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<p>Preencher dados do aluno(a): </p>
+	<fieldset>
+		<legend>Cadastro de Aluno (a)</legend>
+		<form method="post" action="cadastro_aluno.php">
+			<p><label for="aluno">Nome:<br><input type="text" name="aluno" id="aluno"></label></p>
+			<p><label for="cpf">CPF:<br><input type="text" name="cpf" id="cpf"><br>(sem espaços, pontos ou traços)</label></p>
+			
+		</form>
+	</fieldset>
+</body>
+</html>
