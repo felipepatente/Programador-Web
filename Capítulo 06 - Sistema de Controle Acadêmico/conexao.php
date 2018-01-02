7c472787b73ffa59fd2215e0d1a079e06f10ebc3
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 21:00:57 -0200

    Aproveitando o arquivo de conexão do capítulo anterior

---
+++ Capítulo 06 - Sistema de Controle Acadêmico/conexao.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Conexao</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		$conexao = mysql_connect("localhost","root","");
+		if(!$conexao){
+			die('Não foi possível conectar ao banco de dados. Erro detectado: ' . mysql_error());
+		}
+		//echo "Conexão bem-sucedida.";
+		mysql_set_charset('utf-8',$conexao);
+
+		
+
+		/*
+
+		$banco = "CREATE DATABASE sis_academico";
+		//mysql_query($banco);
+
+		if(mysql_query($banco,$conexao)){
+			echo "O banco de dados foi criado com sucesso<br>";
+		}else{
+			echo "Erro ao criar o banco de dados: " . mysql_error();
+		}
+
+		*/
+
+
+		//mysql_close($conexao);
+	?>
+</body>
+</html>
