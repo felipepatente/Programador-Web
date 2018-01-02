33a92e7320b2f1c1d9df8af79c89e36134b0e3c9
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 20:36:16 -0200

    Adicionando o campo estado já com as opções

--- Capítulo 06 - Sistema de Controle Acadêmico/aluno.php
+++ Capítulo 06 - Sistema de Controle Acadêmico/aluno.php
@@ -16,6 +16,39 @@
 			<p><label for="cep">CEP:<br><input type="text" name="cep" id="cep"></label></p>
 			<p><label for="bairro">Bairro:<br><input type="text" name="bairro" id="bairro"></label></p>
 			<p><label for="cidade">Cidade:<br><input type="text" name="cidade" id="cidade"></label></p>
+			<p>
+				<label for="estado">Estado:<br></label>
+				<select name="estado" id="select">
+					<option value="">Selecione o Estado</option>
+					<option value="ac">Acre</option>
+					<option value="al">Alagoas</option>
+					<option value="ap">Amapá</option>
+					<option value="am">Amazonas</option>
+					<option value="ba">Bahia</option>
+					<option value="ce">Ceará</option>
+					<option value="df">Distrito Federal</option>
+					<option value="es">Espírito Santo</option>
+					<option value="go">Goiás</option>
+					<option value="ma">Maranhão</option>
+					<option value="ms">Mato Grosso do Sul</option>
+					<option value="mt">Mato Grosso</option>
+					<option value="mg">Minas Gerais</option>
+					<option value="pa">Pará</option>
+					<option value="pb">Paraíba</option>
+					<option value="pr">Paraná</option>
+					<option value="pe">Pernanbuco</option>
+					<option value="pi">Piauí</option>
+					<option value="rj">Rio de Janeiro</option>
+					<option value="rn">Rio Grande do Norte</option>
+					<option value="rs">Rio Grande do Sul</option>
+					<option value="ro">Rondônia</option>
+					<option value="rr">Roraima</option>
+					<option value="sc">Santa Catarina</option>
+					<option value="sp">São Paulo</option>
+					<option value="se">Sergipe</option>
+					<option value="to">Tocantins</option>
+				</select>
+			</p>
 		</form>
 	</fieldset>
 </body>
