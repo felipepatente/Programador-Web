a1cca1bfd77a7e39ad65bb05b6df27eda4290ff3
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 21:44:30 -0200

    Criando a tabela do professor

--- Capítulo 06 - Sistema de Controle Acadêmico/Sistema Acadêmico.sql
+++ Capítulo 06 - Sistema de Controle Acadêmico/Sistema Acadêmico.sql
@@ -15,3 +15,24 @@ CREATE TABLE aluno(
 SELECT * FROM aluno;
 
 
+CREATE TABLE professor(
+	
+    id_aluno INT PRIMARY KEY AUTO_INCREMENT,
+    cpf VARCHAR(100) NOT NULL,
+    nome VARCHAR (100) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    complemento VARCHAR(100),
+    cep CHAR(8),
+    bairro VARCHAR(25) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,    
+    estado CHAR(2) NOT NULL,
+    telefone CHAR(11),
+    formacao VARCHAR(25),
+    titulacao VARCHAR(10)
+);
+
+SELECT * FROM professor;
+
+
+
+
