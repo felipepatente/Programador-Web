fd1562c227017f767a3929f278f7b26b8dd36a4a
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 20:22:09 -0200

    Colocando os campos de endereço e complemento

--- Capítulo 06 - Sistema de Controle Acadêmico/aluno.php
+++ Capítulo 06 - Sistema de Controle Acadêmico/aluno.php
@@ -11,7 +11,8 @@
 		<form method="post" action="cadastro_aluno.php">
 			<p><label for="aluno">Nome:<br><input type="text" name="aluno" id="aluno"></label></p>
 			<p><label for="cpf">CPF:<br><input type="text" name="cpf" id="cpf"><br>(sem espaços, pontos ou traços)</label></p>
-			
+			<p><label for="endereco">Endereço:<br><input type="text" name="endereco" id="endereco"></label></p>
+			<p><label for="complemento">Complemento:<br><input type="text" name="complemento" id="complemento"></label></p>
 		</form>
 	</fieldset>
 </body>
