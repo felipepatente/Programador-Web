3230902e02f2fbc260928bb28b79b5bea7225326
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 21:47:01 -0200

    Mudando o valor do atributo action

--- Capítulo 06 - Sistema de Controle Acadêmico/professor.php
+++ Capítulo 06 - Sistema de Controle Acadêmico/professor.php
@@ -8,7 +8,7 @@
 	<p>Preencher dados do aluno(a): </p>
 	<fieldset>
 		<legend>Cadastro de Aluno (a)</legend>
-		<form method="post" action="cadastro_aluno.php">
+		<form method="post" action="cadastro_professor.php">
 			<p><label for="aluno">Nome:<br><input type="text" name="aluno" id="aluno"></label></p>
 			<p><label for="cpf">CPF:<br><input type="text" name="cpf" id="cpf"><br>(sem espaços, pontos ou traços)</label></p>
 			<p><label for="endereco">Endereço:<br><input type="text" name="endereco" id="endereco"></label></p>
