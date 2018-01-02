5e48e1026ab024e92b91dfe3583b313b7f763770
felipepatente <felipepatente@yahoo.com.br>
2018-01-02 21:13:38 -0200

    Fazendo ajustes na tabela do aluno

--- Capítulo 06 - Sistema de Controle Acadêmico/Sistema Acadêmico.sql
+++ Capítulo 06 - Sistema de Controle Acadêmico/Sistema Acadêmico.sql
@@ -3,11 +3,15 @@ CREATE TABLE aluno(
     id_aluno INT PRIMARY KEY AUTO_INCREMENT,
     cpf VARCHAR(100) NOT NULL,
     nome VARCHAR (100) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
     complemento VARCHAR(100),
     cep CHAR(8),
     bairro VARCHAR(25) NOT NULL,
-    cidade VARCHAR(50) NOT NULL,
-    estado VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,    
     estado CHAR(2) NOT NULL,
     telefone CHAR(11)
 );
+
+SELECT * FROM aluno;
+
+
